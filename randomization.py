032e6583c977b49f998e813d5bb09a2a5bdfd47a
sebastian122H <sebastian_haidinger@outlook.com>
2023-03-22 22:45:17 +0100

    started randomization file and testing preselection

---
+++ randomization.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import openpyxl
+##  Parsing CSV
+etoro_raw = pd.read_csv("etoro_data.csv")
+stock_list = etoro_raw["Ticker"].values.tolist()
+
+##  random stock selection and group allocation
+stock_num = int(input("How many stocks does every group need?: "))
+
+all_picks = ["SPY"]
+for k in range(1,5):
+    group_picks = np.random.choice(stock_list,stock_num,replace=False)
+    print("Group ", k, "stocks are: ", group_picks)
+    all_picks.extend(group_picks)
